9740f0aa1ff966f9ee7d1a916e092c1cc6bf6ac9
hashemJaber <91439032+hashemJaber@users.noreply.github.com>
2023-01-31 16:48:47 -0800

    Update DNN.py

--- DNN.py
+++ DNN.py
@@ -234,5 +234,5 @@ layer_dims=np.array([2,3,4]   ))
 
 print(str(dnn_1.activations));
 #to do : add a L2 or L1 regulriazation method
-#to do : add a dropout tool for every layer
+#to do : add a dropout hyperparameter tool for every layer
 #to do: add a Adam optimization and minibatching hyper parameters
